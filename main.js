bbc62736c9d589e3a1c25a2a039e58999d22c1c0
Hamish <hamisharro@icloud.com>
2021-08-13 14:37:41 +0100

    add essentials to project

---
+++ main.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGlRenderer({
+  canvas: document.querySelector("#bg"),
+});
