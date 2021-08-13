dcd943e760933ad6fbe9cbdb0730a01a8185f3e5
Hamish <hamisharro@icloud.com>
2021-08-13 15:21:26 +0100

    change versions

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import "./style.css";
-
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
@@ -18,7 +17,6 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 renderer.render(scene, camera);
-
-// const loader = new OBJLoader();
