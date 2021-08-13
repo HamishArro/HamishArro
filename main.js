a925e4712f86dc9a507d1b636caab503ff99bc45
Hamish <hamisharro@icloud.com>
2021-08-13 16:31:03 +0100

    add helpers to main.js

--- main.js
+++ main.js
@@ -35,6 +35,10 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 
 scene.add(pointLight, ambientLight);
 
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
 function animate() {
   requestAnimationFrame(animate);
 
