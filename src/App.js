c261b1947f432feb8f1277f8d5112cc58b8582bc
Hamish <hamisharro@icloud.com>
2021-08-23 23:33:34 +0100

    added scene to project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
+
+const Scene = () => {
+  return <Canvas></Canvas>;
+};
+
+const App = () => {
+  return <Scene />;
+};
 
 export default App;
