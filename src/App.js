9c19267fe34d12f8806ddc2fa429ee03947d06c3
Hamish <hamisharro@icloud.com>
2021-08-26 13:01:15 +0100

    add Loader() to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { Canvas } from "@react-three/fiber";
-import React from "react";
-import loadModel from "./loadModel.js";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { Html, useProgress } from "@react-three/drei";
+import { Suspense } from "react";
 import "./App.css";
 
-const App = () => {
+export default function App() {
   return (
     <Canvas>
       <ambientLight />
+      <Suspense fallback={<Loader />}>
+        <Eyeball />
+      </Suspense>
     </Canvas>
   );
-};
+}
 
-export default App;
+function Loader() {
+  const { progress } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
+}
