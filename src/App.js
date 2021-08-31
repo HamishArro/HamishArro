88fdf69b648f1809ca5ffc9921e4c4b0ca977604
Hamish <hamisharro@icloud.com>
2021-08-31 13:22:38 +0100

    attempt to use old JS to load object

--- src/App.js
+++ src/App.js
@@ -1,87 +1,52 @@
 import { Canvas } from "@react-three/fiber";
 import React, { useRef, useEffect, Suspense } from "react";
 import * as THREE from "three";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 import Eye from "./components/Eye";
 import "./App.css";
 
-const coords = new THREE.Vector2(-1, -1);
-
 const App = () => {
   const canvasContainer = useRef();
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  const renderer = new THREE.WebGLRenderer();
+  const ambientLight = new THREE.AmbientLight(0xffffff);
 
   useEffect(() => {
     createWorld();
+    scene.add(ambientLight);
+    loadModel("/public/eyeball/eyeball").then((eyeball) => {
+      scene.add(eyeball);
+      console.log("loaded object");
+    });
   }, []);
 
-  /**
-   * Simple demo setup of a Threejs scene
-   */
-
   const createWorld = () => {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-
-    var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     canvasContainer.current.appendChild(renderer.domElement);
-
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    var raycaster = new THREE.Raycaster();
-
-    camera.position.z = 5;
-
-    var animate = function () {
+    camera.position.setZ(30);
+    function animate() {
       requestAnimationFrame(animate);
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      // raycasting
-
-      raycaster.setFromCamera(coords, camera);
-      var intersects = raycaster.intersectObject(cube);
-
-      if (intersects.length > 0) {
-        cube.material.color.set(0xff0000);
-      } else {
-        cube.material.color.set(0x00ff00);
-      }
-
       renderer.render(scene, camera);
-    };
-
+    }
     animate();
   };
 
-  /**
-   * Update the stat ewith the mouse position
-   *
-   */
+  async function loadModel(url) {
+    const objLoader = new OBJLoader();
+    const mtlLoader = new MTLLoader();
+    var materials = await mtlLoader.loadAsync(`${url}.mtl`);
+    objLoader.setMaterials(materials);
+    return objLoader.loadAsync(`${url}.obj`);
+  }
 
-  const handleMouseMove = (event) => {
-    event.preventDefault();
-    coords.x = (event.clientX / window.innerWidth) * 2 - 1;
-    coords.y = -(event.clientY / window.innerHeight) * 2 + 1;
-  };
-
-  return (
-    <>
-      <div
-        onMouseMove={handleMouseMove}
-        className="container"
-        ref={canvasContainer}
-      />
-    </>
-  );
+  return <div className="container" ref={canvasContainer} />;
 };
 
 export default App;
