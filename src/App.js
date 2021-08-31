c54672d7ac88512a5d5d82e6a5edda67c5801367
Hamish <hamisharro@icloud.com>
2021-08-31 13:43:54 +0100

    change back to using react three fiber

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
 import { Canvas } from "@react-three/fiber";
-import React, { useRef, useEffect, Suspense } from "react";
-import * as THREE from "three";
-import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
 import Eye from "./components/Eye";
 import "./App.css";
 
-const App = () => {
-  const canvasContainer = useRef();
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  const renderer = new THREE.WebGLRenderer();
-  const ambientLight = new THREE.AmbientLight(0xffffff);
-
-  useEffect(() => {
-    createWorld();
-    scene.add(ambientLight);
-    loadModel("/public/eyeball/eyeball").then((eyeball) => {
-      scene.add(eyeball);
-      console.log("loaded object");
-    });
-  }, []);
-
-  const createWorld = () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    canvasContainer.current.appendChild(renderer.domElement);
-    camera.position.setZ(30);
-    function animate() {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-    }
-    animate();
+export default function App() {
+  const handleMouseMove = (event) => {
+    event.preventDefault();
+    console.log("moved");
   };
 
-  async function loadModel(url) {
-    const objLoader = new OBJLoader();
-    const mtlLoader = new MTLLoader();
-    var materials = await mtlLoader.loadAsync(`${url}.mtl`);
-    objLoader.setMaterials(materials);
-    return objLoader.loadAsync(`${url}.obj`);
-  }
-
-  return <div className="container" ref={canvasContainer} />;
-};
-
-export default App;
+  return (
+    <div id="canvasContainer">
+      <Canvas>
+        <Suspense fallback={null}>
+          <ambientLight />
+          <Eye scale={100} />
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Suspense } from "react";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={null}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
