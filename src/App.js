aaf668d22c509251fdce8688491cf625e518f899
Hamish <hamisharro@icloud.com>
2021-09-01 22:39:03 +0100

    add handleMove to div which allows fuction to work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Suspense, useEffect, useRef } from "react";
+import * as THREE from "three";
 import Eye from "./components/Eye";
 import "./App.css";
+
+var mouse = new THREE.Vector2();
 
 export default function App() {
   const eye = useRef();
 
   useEffect(() => {
-    addRaycaster();
     console.log(eye);
   });
 
-  const addRaycaster = () => {};
+  const handleMove = (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    console.log(mouse.x);
+    console.log(mouse.y);
+  };
 
   return (
-    <div>
+    <div onMouseMove={handleMove}>
       <Canvas>
         <Suspense fallback={null}>
           <ambientLight />
