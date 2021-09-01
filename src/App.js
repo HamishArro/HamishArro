bacce02a2ab933579d684a44db9ea155479a0906
Hamish <hamisharro@icloud.com>
2021-09-01 23:34:58 +0100

    add animate and scene to create dynamic scene

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Suspense, useEffect, useRef } from "react";
 import * as THREE from "three";
 import Eye from "./components/Eye";
 import "./App.css";
 
 var mouse = new THREE.Vector2();
+var raycaster = new THREE.Raycaster();
+var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -10);
+var pointOfIntersection = new THREE.Vector3();
 
 export default function App() {
-  const eye = useRef();
-
-  useEffect(() => {
-    console.log(eye);
-  });
-
   const handleMove = (event) => {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    console.log(mouse.x);
-    console.log(mouse.y);
   };
 
   return (
     <div onMouseMove={handleMove}>
       <Canvas>
-        <Suspense fallback={null}>
-          <ambientLight />
-          <mesh ref={eye}>
-            <Eye scale={100} />
-          </mesh>
-          <OrbitControls />
-        </Suspense>
+        <Scene />
       </Canvas>
     </div>
+  );
+}
+
+function Scene() {
+  const eye = useRef();
+  const { camera } = useThree();
+
+  useEffect(() => {
+    console.log(eye);
+    console.log(camera);
+    animate();
+  });
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    raycaster.setFromCamera(mouse, camera);
+    raycaster.ray.intersectPlane(plane, pointOfIntersection);
+  };
+
+  return (
+    <Suspense fallback={null}>
+      <ambientLight />
+      <mesh ref={eye}>
+        <Eye scale={100} />
+      </mesh>
+    </Suspense>
   );
 }
