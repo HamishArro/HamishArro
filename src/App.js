5887bfb7a9b8d90b5243ecd67171df13b081ce06
Hamish <hamisharro@icloud.com>
2021-09-10 11:59:28 +0100

    amlost finish effect for following mouse, need to figure out what point of intersection isn't being worked out correctly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Suspense, useEffect, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 import * as THREE from "three";
 import Eye from "./components/Eye";
 import "./App.css";
 
-var mouse = new THREE.Vector2();
-var raycaster = new THREE.Raycaster();
-var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -10);
-var pointOfIntersection = new THREE.Vector3();
-
 export default function App() {
+  const mouse = new THREE.Vector2();
+
   const handleMove = (event) => {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -18,35 +15,13 @@ export default function App() {
   return (
     <div onMouseMove={handleMove}>
       <Canvas>
-        <Scene />
+        <Suspense fallback={null}>
+          <ambientLight />
+          <mesh>
+            <Eye coords={mouse} scale={100} />
+          </mesh>
+        </Suspense>
       </Canvas>
     </div>
-  );
-}
-
-function Scene() {
-  const eye = useRef();
-  const { camera } = useThree();
-
-  useEffect(() => {
-    console.log(eye);
-    console.log(camera);
-    animate();
-  });
-
-  const animate = () => {
-    requestAnimationFrame(animate);
-    raycaster.setFromCamera(mouse, camera);
-    raycaster.ray.intersectPlane(plane, pointOfIntersection);
-    eye.current.lookAt(pointOfIntersection);
-  };
-
-  return (
-    <Suspense fallback={null}>
-      <ambientLight />
-      <mesh ref={eye}>
-        <Eye scale={100} />
-      </mesh>
-    </Suspense>
   );
 }
