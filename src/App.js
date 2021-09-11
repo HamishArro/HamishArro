63adcb8c0599a36891852f7f459fd17457bce5a0
Hamish <hamisharro@icloud.com>
2021-09-11 18:29:47 +0100

    add react router dom to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import * as THREE from "three";
 import Eye from "./components/Eye";
 import "./App.css";
@@ -17,15 +18,17 @@ export default function App() {
   };
 
   return (
-    <div onMouseMove={handleMove}>
-      <Canvas>
-        <Suspense fallback={null}>
-          <ambientLight />
-          <mesh onClick={eyeClick}>
-            <Eye coords={mouse} />
-          </mesh>
-        </Suspense>
-      </Canvas>
-    </div>
+    <Route exact path="/">
+      <div onMouseMove={handleMove}>
+        <Canvas>
+          <Suspense fallback={null}>
+            <ambientLight />
+            <mesh onClick={eyeClick}>
+              <Eye coords={mouse} />
+            </mesh>
+          </Suspense>
+        </Canvas>
+      </div>
+    </Route>
   );
 }
