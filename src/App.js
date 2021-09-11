ba00cb50be858a693ca13ee99f74576b2e7ba689
Hamish <hamisharro@icloud.com>
2021-09-11 18:16:54 +0100

    add eyeClick to App()

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import "./App.css";
 export default function App() {
   const mouse = new THREE.Vector2();
 
+  const eyeClick = () => {
+    console.log("clicked");
+  };
+
   const handleMove = (event) => {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -17,7 +21,7 @@ export default function App() {
       <Canvas>
         <Suspense fallback={null}>
           <ambientLight />
-          <mesh>
+          <mesh onClick={eyeClick}>
             <Eye coords={mouse} />
           </mesh>
         </Suspense>
