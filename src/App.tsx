bfbcc8676a64bc8e36d6af2e5be65536567604d4
Hamish Arro <hamisharro@Hamishs-MacBook-Pro.local>
2022-03-26 23:56:15 +0000

    get app working using typescrpit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Eye from "./components/Eye";
 
 function App() {
+  const mouse = new THREE.Vector2();
+
+  const handleMove = (event: React.MouseEvent) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onMouseMove={handleMove}>
+      <Canvas>
+        <Suspense fallback={null}>
+          <ambientLight />
+          <mesh>
+            <Eye coords={mouse} />
+          </mesh>
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
--- src/components/Eye.tsx
+++ src/components/Eye.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
-import './App.css';
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useGLTF } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import * as THREE from 'three';
 
-function Eye() {
+function Eye(props: { coords: THREE.Vector2 } ) {
+    const group = useRef<THREE.Object3D>();
+    const { camera } = useThree();
+    var raycaster = new THREE.Raycaster();
+    var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -10);
+    var pointOfIntersection = new THREE.Vector3();
+    camera.position.z = 30;
 
     useEffect(() => {
-        AnimationEffect
-    }) 
+        animate();
+    });
+    
+    const animate = () => {
+        requestAnimationFrame(animate);
+        raycaster.setFromCamera(props.coords, camera);
+        raycaster.ray.intersectPlane(plane, pointOfIntersection);
+        group.current?.children[0].lookAt(pointOfIntersection);
+    };
+
+    const { nodes, materials } = useGLTF("/eye.glb");
+    
 
     return  (
-
+        <group ref={group} {...props} dispose={null}>
+            <primitive
+                scale={200}
+                object={nodes.eye_low001}
+                material={materials.Eye_material}
+            />
+        </group>
     );
 }
 
-export default App;
+export default Eye;
