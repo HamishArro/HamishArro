c57db4d02608553f94331e36151b333533899b8f
Hamish Arro <hamisharro@Hamishs-MacBook-Pro.local>
2022-03-26 00:19:06 +0000

    import typescript project and start conversion

---
+++ src/components/Eye.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { useEffect, useRef } from 'react';
+
+function Eye() {
+
+    useEffect(() => {
+        AnimationEffect
+    }) 
+
+    return  (
+
+    );
+}
+
+export default App;
