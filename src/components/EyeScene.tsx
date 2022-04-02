baf3492397fa908300a361c337da684f964d893c
Hamish Arro <hamisharro@icloud.com>
2022-04-02 21:23:55 +0100

    add touch move

--- src/components/EyeScene.tsx
+++ src/components/EyeScene.tsx
@@ -5,19 +5,31 @@ import { Suspense } from 'react'
 import Eye from './Eye'
 
 function EyeScene() {
-    const mouse = new THREE.Vector2()
+    const coords = new THREE.Vector2()
 
-    const handleMove = (event: React.MouseEvent) => {
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+    const setCoords = (x: number, y: number) => {
+        coords.x = (x / window.innerWidth) * 2 - 1
+        coords.y = -(y / window.innerHeight) * 2 + 1
+    }
+
+    const handleMouseMove = (event: React.MouseEvent) => {
+        setCoords(event.clientX, event.clientY)
+    }
+
+    const handleTouchMove = (event: React.TouchEvent) => {
+        setCoords(event.touches[0].clientX, event.touches[0].clientY)
     }
 
     return (
-        <div onMouseMove={handleMove}>
+        <div
+            onMouseMove={handleMouseMove}
+            onTouchMove={handleTouchMove}
+            onTouchStart={handleTouchMove}
+        >
             <Canvas>
                 <Suspense fallback={null}>
                     <ambientLight />
-                    <Eye coords={mouse} />
+                    <Eye coords={coords} />
                 </Suspense>
             </Canvas>
         </div>
